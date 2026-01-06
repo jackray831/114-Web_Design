d25bdd2a29aae549d06fd6900e4d3cb38b2a3790
Zhou Yi Cheng <jackray831@gmail.com>
2026-01-07 03:26:27 +0800

    Fix Backend host IP

--- Backend/main.py
+++ Backend/main.py
@@ -673,4 +673,4 @@ async def serve_spa(full_path: str):
 
 # 允許在 Python 腳本中直接執行
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
